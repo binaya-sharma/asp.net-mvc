ec73492fcc8c4f797fae7bf7365ac99227690c38
binaya sharma <mebinayasharma@gmail.com>
2022-03-29 22:09:07 +0545

    Model-View-Controller

---
+++ wenesday/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using wenesday.Models;
+
+namespace wenesday.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<wenesday.Models.Product> Product { get; set; }
+        public DbSet<wenesday.Models.Customer> Customer { get; set; }
+    }
+}
---
+++ wenesday/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace wenesday.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
